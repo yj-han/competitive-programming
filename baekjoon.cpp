d59a6edd85179403566de3d69a9defb033a7c9c0
Yeji Han <yejihanc@gmail.com>
2023-01-12 10:40:33 +0900

    Added a cpp file for acmicpc

---
+++ baekjoon.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+#include <vector>
+#include <set>
+#include <cmath>
+#include <algorithm>
+#include <cstdio>
+#include <stack>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    return 0;
+}
