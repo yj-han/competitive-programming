cb4e6da4eb44169c83340c353174720d193f2738
Yeji Han <yejihanc@gmail.com>
2023-02-14 02:35:10 +0900

    Removed unnecessary files

---
+++ playground.cpp
@@ -0,0 +1,54 @@
+// C. Least Prefix Sum
+#include <iostream>
+#include <vector>
+#include <set>;
+
+using namespace std;
+using ll = long long;
+
+int solve(vector<int> a) {
+    ll sum = 0;
+    multiset<int> s;
+    int ret = 0;
+
+    for (int x : a) {
+        sum += x;
+        s.insert(x);
+
+        while (sum < 0) {
+            sum -= 2 * (*s.begin());
+            ret += 1;
+            s.erase(s.begin());
+        }
+    }
+
+    return ret;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    
+    int t; cin >> t;
+
+    while (t--) {
+        int n, m; cin >> n >> m;
+        vector<int> a(n);
+        for (int i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        vector<int> b;
+        for (int i = m; i < n; i++) {
+            b.push_back(a[i]);
+        }
+
+        vector<int> c;
+        for (int i = m - 1; i > 0; i--)
+            c.push_back(-a[i]);
+
+        cout << solve(b) + solve(c) << '\n';
+    }
+
+    return 0;
+}
